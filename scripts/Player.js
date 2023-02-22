7e62b5ac4eba8f43b6e1ffd65ec20fcb724f93f8
Soufiane Elkhamlichi <05soufiane@gmail.com>
2023-02-22 15:47:18 +0100

    checkCollision & obstacleCollisionHandler funcs

--- scripts/Player.js
+++ scripts/Player.js
@@ -8,17 +8,38 @@ export class Player extends Object
 		super.collisionX = this.game.width * .5;
 		super.collisionY = this.game.height * .5;
 		super.collisionRadius = 30;
-		super.speedModifier = 50;
+		super.speedModifier = 30;
+		super.image = document.getElementById('bull');
+		this.spriteWidth = 255;
+		this.spriteHeight = 255;
+		super.width = this.spriteWidth;
+		super.height = this.spriteHeight;
 	}
 
 	draw(context)
 	{
+		context.drawImage(this.image, this.collisionX, this.collisionY);
 		super.draw(context);
 		context.beginPath();
 		context.moveTo(this.collisionX, this.collisionY);
 		context.lineTo(this.game.mouse.x, this.game.mouse.y);
 		context.stroke();
 	}
+
+	obstacleCollisionHandler()
+	{
+		this.game.obstacles.forEach(obstacle => {
+			let	collisionInfo = this.game.checkCollision(obstacle, this);
+			if (collisionInfo['isColliding'])
+			{
+				const	unit_x = collisionInfo['deltaX'] / collisionInfo['distance'];
+				const	unit_y = collisionInfo['deltaY'] / collisionInfo['distance'];
+				this.collisionX = obstacle.collisionX - (collisionInfo['sumOfRadius'] + 1) * unit_x;
+				this.collisionY = obstacle.collisionY - (collisionInfo['sumOfRadius'] + 1) * unit_y;
+			}
+		});
+	}
+
 	update()
 	{
 		this.deltaX = this.game.mouse.x - this.collisionX;
@@ -36,5 +57,7 @@ export class Player extends Object
 		}
 		this.collisionX += this.speedX * this.speedModifier;
 		this.collisionY += this.speedY * this.speedModifier;
+
+		this.obstacleCollisionHandler();
 	}
 }
