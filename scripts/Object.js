52f2e6a829fa302002546f97517bb61702617e0c
Soufiane Elkhamlichi <05soufiane@gmail.com>
2023-02-22 03:36:20 +0100

    draw random image for each obstacle

--- scripts/Object.js
+++ scripts/Object.js
@@ -3,13 +3,20 @@ export class Object
 	constructor(game)
 	{
 		this.game = game;
-		this.collisionX;
-		this.collisionY;
+		this.collisionX = 0;
+		this.collisionY = 0;
 		this.collisionRadius = 1;
 		this.speedX = 0;
 		this.speedY = 0;
-		this.deltaX;
-		this.deltaY;
+		this.deltaX = 0;
+		this.deltaY = 0;
+		this.image = null;
+		this.spriteWidth = 0;
+		this.spriteHeight = 0;
+		this.width = this.spriteWidth;
+		this.height = this.spriteHeight;
+		this.spriteX = 0;
+		this.spriteY = 0;
 	}
 
 	draw(context)
--- scripts/Obstacle.js
+++ scripts/Obstacle.js
@@ -2,16 +2,26 @@ import { Object } from "./Object.js";
 
 export class Obstacle extends Object
 {
-	constructor(name)
+	constructor(game)
 	{
-		super(name);
-		this.collisionX = Math.random() * this.game.width;
-		this.collisionY = Math.random() * this.game.height;
-		this.collisionRadius = 60;
+		super(game);
+		super.collisionX = Math.random() * this.game.width;
+		super.collisionY = Math.random() * this.game.height;
+		super.collisionRadius = 50;
+		super.image = document.getElementById('obstacles');
+		super.spriteWidth = 250;
+		super.spriteHeight = 250;
+		super.width = this.spriteWidth;
+		super.height = this.spriteHeight;
+		super.spriteX = this.collisionX - this.width * .5;
+		super.spriteY = this.collisionY - this.width * .5 - 70;
+		this.frameX = Math.floor(Math.random() * 4);
+		this.frameY = Math.floor(Math.random() * 3);
 	}
 
 	draw(context)
 	{
+		context.drawImage(this.image, this.frameX * this.spriteWidth, this.frameY * this.spriteHeight, this.spriteWidth, this.spriteHeight, this.spriteX, this.spriteY, this.width, this.height);
 		super.draw(context);
 	}
 }
