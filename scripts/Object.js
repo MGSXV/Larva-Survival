49fda88ecb60d096f2f0108e9a225055c42bd191
Soufiane Elkhamlichi <05soufiane@gmail.com>
2023-02-22 01:27:37 +0100

    draw function for object class

--- scripts/Object.js
+++ scripts/Object.js
@@ -1,14 +1,29 @@
 export class Object
 {
-	constructor()
+	constructor(game)
 	{
+		this.game = game;
 		this.collisionX;
 		this.collisionY;
 		this.collisionRadius = 1;
+		this.speedX = 0;
+		this.speedY = 0;
+		this.deltaX;
+		this.deltaY;
 	}
 
 	draw(context)
 	{
-		
+		context.beginPath();
+		context.arc(this.collisionX, this.collisionY, this.collisionRadius, 0, Math.PI * 2);
+		context.save();
+		context.globalAlpha = .5;
+		context.fill();
+		context.restore();
+		context.stroke();
+	}
+	update()
+	{
+
 	}
 }
