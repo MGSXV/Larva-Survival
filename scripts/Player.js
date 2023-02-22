72646fca178cadc9647d9c03b6331cbb1e9160b7
Soufiane Elkhamlichi <05soufiane@gmail.com>
2023-02-22 01:27:20 +0100

    draw, update function

--- scripts/Player.js
+++ scripts/Player.js
@@ -4,21 +4,37 @@ export class Player extends Object
 {
 	constructor(game)
 	{
-		super();
-		this.game = game;
+		super(game);
 		super.collisionX = this.game.width * .5;
 		super.collisionY = this.game.height * .5;
 		super.collisionRadius = 30;
+		this.speedModifier = 50;
 	}
 
 	draw(context)
 	{
+		super.draw(context);
 		context.beginPath();
-		context.arc(this.collisionX, this.collisionY, this.collisionRadius, 0, Math.PI * 2);
-		context.save();
-		context.globalAlpha = .5;
-		context.fill();
-		context.restore();
+		context.moveTo(this.collisionX, this.collisionY);
+		context.lineTo(this.game.mouse.x, this.game.mouse.y);
 		context.stroke();
+	}
+	update()
+	{
+		this.deltaX = this.game.mouse.x - this.collisionX;
+		this.deltaY = this.game.mouse.y - this.collisionY;
+		const	distance = Math.hypot(this.deltaY, this.deltaX);
+		if (distance > this.speedModifier)
+		{
+			this.speedX = this.deltaX / distance || 0;
+			this.speedY = this.deltaY / distance || 0;
+		}
+		else
+		{
+			this.speedX = 0;
+			this.speedY = 0;
+		}
+		this.collisionX += this.speedX * this.speedModifier;
+		this.collisionY += this.speedY * this.speedModifier;
 	}
 }
